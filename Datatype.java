671c62a336bebedb89c97502b47bf8102db7b668
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:02:00 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,28 @@
+public class Datatype {
+        public static void main(String[] args){
+            byte b=100;
+            short s=145;
+            int v=67;
+            int calc=-964320;
+            long amountVal=1234567891;
+            float intrestRate=21.56f;
+            double sineVal=12345.678d;
+            boolean flag=true;
+            boolean val=false;
+            char ch1=88;
+            char ch2='Y';
+            System.out.println("byte Value="+b);
+            System.out.println("short Value="+s);
+            System.out.println("int Value="+v);
+            System.out.println("int second Value="+calc);
+            System.out.println("long Value="+amountVal);
+            System.out.println("flaot Value="+intrestRate);
+            System.out.println("double Value="+sineVal);
+            System.out.println("boolean Value="+flag);
+            System.out.println(" boolean Value="+val);
+            System.out.println("char Value="+ch1);
+            System.out.println("char Value="+ch2);
+        }
+    }
+    
+
